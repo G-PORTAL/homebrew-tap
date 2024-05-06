f9dbfd4d93aa81e5df7615505c3139357636eaa0
Alexander Birkner <alex.birkner@gmail.com>
2024-05-06 13:01:00 +0200

    chore(gpcore-cli): Update to 24.0.0

--- Casks/g/gpcore.rb
+++ Casks/g/gpcore.rb
@@ -1,9 +1,9 @@
 cask "gpcore" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "24.1.0"
-  sha256 arm:   "a0a6330e0958d22285c5e2567a36ad9f180f4cfacf23e4762c0f04f4021f15ee",
-         intel: "621ec63929395e65586af4db8d49021fc9e9b37e506f288a47842f18272d50f8"
+  version "24.0.0"
+  sha256 arm:   "16ac8242f3abab383d504f3effcd545c72643c5e4c5d15205373f4fd0ca62500",
+         intel: "d5f5c38e49ea25134229ad89acf5414bfddff7fb9d3d7994ca3be6335fb94533"
 
   url "https://github.com/G-PORTAL/gpcore-cli/releases/download/v#{version}/gpcore_darwin_#{arch}.tar.gz",
       verified: "github.com/G-PORTAL/gpcore-cli/"
